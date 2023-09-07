3dd743904cfb7bd53db015ad721f37d75d87e016
Bobby Yu <90119496+byu99@users.noreply.github.com>
2023-09-06 22:19:12 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import { Layout, Typograhpy, Space } from "antd";
+import { Layout, Typography, Space } from "antd";
 
 import {
     Navbar,
@@ -41,9 +41,23 @@ const App = () => {
                         </Routes>
                     </div>
                 </Layout>
-            </div>
 
-            <div className="footer"></div>
+                <div className="footer">
+                    <Typography.Title
+                        level={5}
+                        style={{ color: "white", textAlign: "center" }}
+                    >
+                        Cryptoverse <br />
+                        All rights reserved
+                    </Typography.Title>
+
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
+            </div>
         </div>
     );
 };
